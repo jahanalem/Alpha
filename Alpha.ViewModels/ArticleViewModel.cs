31a91ab1cd985e6e60fbb7e3c5ed90aa7d80bf16
jahanalem <s.r.alem19@gmail.com>
2020-03-06 10:51:19 +0100

    a1

--- Alpha.ViewModels/ArticleViewModel.cs
+++ Alpha.ViewModels/ArticleViewModel.cs
@@ -6,7 +6,15 @@ namespace Alpha.ViewModels
     public class ArticleViewModel : BaseViewModel
     {
         public Article Article { get; set; }
+
+        /// <summary>
+        /// Just only related tags
+        /// </summary>
         public List<Tag> Tags { get; set; }
-        public List<Tag> AllOfTags { get; set; }
+
+        /// <summary>
+        /// Related tags in the set of all tags
+        /// </summary>
+        public List<Tag> AllTags { get; set; }
     }
 }
