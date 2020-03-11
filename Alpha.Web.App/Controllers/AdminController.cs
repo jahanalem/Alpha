ef58af927974fe4b5265a38d6c795cd01b985e41
jahanalem <s.r.alem19@gmail.com>
2020-03-11 22:32:24 +0100

    some bugs fixed

--- Alpha.Web.App/Controllers/AdminController.cs
+++ Alpha.Web.App/Controllers/AdminController.cs
@@ -7,147 +7,147 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Alpha.Web.App.Controllers
 {
-    [Authorize(Roles = "Admins")]
-    public class AdminController : Controller
-    {
-        private UserManager<User> userManager;
-        private IUserValidator<User> userValidator;
-        private IPasswordValidator<User> passwordValidator;
-        private IPasswordHasher<User> passwordHasher;
-        public AdminController(UserManager<User> usrMgr,
-            IUserValidator<User> userValid,
-            IPasswordValidator<User> passValid,
-            IPasswordHasher<User> passwordHash)
-        {
-            userManager = usrMgr;
-            userValidator = userValid;
-            passwordValidator = passValid;
-            passwordHasher = passwordHash;
-        }
+    //[Authorize(Roles = "Admins")]
+    //public class AdminController : Controller
+    //{
+    //    private UserManager<User> userManager;
+    //    private IUserValidator<User> userValidator;
+    //    private IPasswordValidator<User> passwordValidator;
+    //    private IPasswordHasher<User> passwordHasher;
+    //    public AdminController(UserManager<User> usrMgr,
+    //        IUserValidator<User> userValid,
+    //        IPasswordValidator<User> passValid,
+    //        IPasswordHasher<User> passwordHash)
+    //    {
+    //        userManager = usrMgr;
+    //        userValidator = userValid;
+    //        passwordValidator = passValid;
+    //        passwordHasher = passwordHash;
+    //    }
         
-        public ViewResult Index() => View(userManager.Users);
+    //    public ViewResult Index() => View(userManager.Users);
 
-        public ViewResult Create() => View();
-        [HttpPost]
-        public async Task<IActionResult> Create(CreateUserViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                User user = new User
-                {
-                    UserName = model.Name,
-                    Email = model.Email
-                };
-                IdentityResult result
-                    = await userManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index");
-                }
-                else
-                {
-                    foreach (IdentityError error in result.Errors)
-                    {
-                        ModelState.AddModelError("", error.Description);
-                    }
-                }
-            }
-            return View(model);
-        }
+    //    public ViewResult Create() => View();
+    //    [HttpPost]
+    //    public async Task<IActionResult> Create(CreateUserViewModel model)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            User user = new User
+    //            {
+    //                UserName = model.Name,
+    //                Email = model.Email
+    //            };
+    //            IdentityResult result
+    //                = await userManager.CreateAsync(user, model.Password);
+    //            if (result.Succeeded)
+    //            {
+    //                return RedirectToAction("Index");
+    //            }
+    //            else
+    //            {
+    //                foreach (IdentityError error in result.Errors)
+    //                {
+    //                    ModelState.AddModelError("", error.Description);
+    //                }
+    //            }
+    //        }
+    //        return View(model);
+    //    }
 
-        [HttpPost]
-        public async Task<IActionResult> Delete(string id)
-        {
-            User user = await userManager.FindByIdAsync(id);
-            if (user != null)
-            {
-                IdentityResult result = await userManager.DeleteAsync(user);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index");
-                }
-                else
-                {
-                    AddErrorsFromResult(result);
-                }
-            }
-            else
-            {
-                ModelState.AddModelError("", "User Not Found");
-            }
-            return View("Index", userManager.Users);
-        }
-        private void AddErrorsFromResult(IdentityResult result)
-        {
-            foreach (IdentityError error in result.Errors)
-            {
-                ModelState.AddModelError("", error.Description);
-            }
-        }
+    //    [HttpPost]
+    //    public async Task<IActionResult> Delete(string id)
+    //    {
+    //        User user = await userManager.FindByIdAsync(id);
+    //        if (user != null)
+    //        {
+    //            IdentityResult result = await userManager.DeleteAsync(user);
+    //            if (result.Succeeded)
+    //            {
+    //                return RedirectToAction("Index");
+    //            }
+    //            else
+    //            {
+    //                AddErrorsFromResult(result);
+    //            }
+    //        }
+    //        else
+    //        {
+    //            ModelState.AddModelError("", "User Not Found");
+    //        }
+    //        return View("Index", userManager.Users);
+    //    }
+    //    private void AddErrorsFromResult(IdentityResult result)
+    //    {
+    //        foreach (IdentityError error in result.Errors)
+    //        {
+    //            ModelState.AddModelError("", error.Description);
+    //        }
+    //    }
 
 
-        public async Task<IActionResult> Edit(string id)
-        {
-            User user = await userManager.FindByIdAsync(id);
-            if (user != null)
-            {
-                return View(user);
-            }
-            else
-            {
-                return RedirectToAction("Index");
-            }
-        }
-        [HttpPost]
-        public async Task<IActionResult> Edit(string id, string email,
-            string password)
-        {
-            User user = await userManager.FindByIdAsync(id);
-            if (user != null)
-            {
-                user.Email = email;
-                IdentityResult validEmail
-                    = await userValidator.ValidateAsync(userManager, user);
-                if (!validEmail.Succeeded)
-                {
-                    AddErrorsFromResult(validEmail);
-                }
-                IdentityResult validPass = null;
-                if (!string.IsNullOrEmpty(password))
-                {
-                    validPass = await passwordValidator.ValidateAsync(userManager,
-                        user, password);
-                    if (validPass.Succeeded)
-                    {
-                        user.PasswordHash = passwordHasher.HashPassword(user,
-                            password);
-                    }
-                    else
-                    {
-                        AddErrorsFromResult(validPass);
-                    }
-                }
-                if ((validEmail.Succeeded && validPass == null)
-                    || (validEmail.Succeeded
-                        && password != string.Empty && validPass.Succeeded))
-                {
-                    IdentityResult result = await userManager.UpdateAsync(user);
-                    if (result.Succeeded)
-                    {
-                        return RedirectToAction("Index");
-                    }
-                    else
-                    {
-                        AddErrorsFromResult(result);
-                    }
-                }
-            }
-            else
-            {
-                ModelState.AddModelError("", "User Not Found");
-            }
-            return View(user);
-        }
+    //    public async Task<IActionResult> Edit(string id)
+    //    {
+    //        User user = await userManager.FindByIdAsync(id);
+    //        if (user != null)
+    //        {
+    //            return View(user);
+    //        }
+    //        else
+    //        {
+    //            return RedirectToAction("Index");
+    //        }
+    //    }
+    //    [HttpPost]
+    //    public async Task<IActionResult> Edit(string id, string email,
+    //        string password)
+    //    {
+    //        User user = await userManager.FindByIdAsync(id);
+    //        if (user != null)
+    //        {
+    //            user.Email = email;
+    //            IdentityResult validEmail
+    //                = await userValidator.ValidateAsync(userManager, user);
+    //            if (!validEmail.Succeeded)
+    //            {
+    //                AddErrorsFromResult(validEmail);
+    //            }
+    //            IdentityResult validPass = null;
+    //            if (!string.IsNullOrEmpty(password))
+    //            {
+    //                validPass = await passwordValidator.ValidateAsync(userManager,
+    //                    user, password);
+    //                if (validPass.Succeeded)
+    //                {
+    //                    user.PasswordHash = passwordHasher.HashPassword(user,
+    //                        password);
+    //                }
+    //                else
+    //                {
+    //                    AddErrorsFromResult(validPass);
+    //                }
+    //            }
+    //            if ((validEmail.Succeeded && validPass == null)
+    //                || (validEmail.Succeeded
+    //                    && password != string.Empty && validPass.Succeeded))
+    //            {
+    //                IdentityResult result = await userManager.UpdateAsync(user);
+    //                if (result.Succeeded)
+    //                {
+    //                    return RedirectToAction("Index");
+    //                }
+    //                else
+    //                {
+    //                    AddErrorsFromResult(result);
+    //                }
+    //            }
+    //        }
+    //        else
+    //        {
+    //            ModelState.AddModelError("", "User Not Found");
+    //        }
+    //        return View(user);
+    //    }
 
-    }
+    //}
 }
