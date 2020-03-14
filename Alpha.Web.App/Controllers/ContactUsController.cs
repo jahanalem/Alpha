d034875ebb7e9d5a4e89c475baa3acfaf6dbd59b
jahanalem <s.r.alem19@gmail.com>
2020-03-14 03:08:22 +0100

    contact form

---
+++ Alpha.Web.App/Controllers/ContactUsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alpha.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alpha.Web.App.Controllers
+{
+    public class ContactUsController : BaseController
+    {
+        public ContactUsController()
+        {
+
+        }
+
+        [HttpGet, AllowAnonymous]
+        public ActionResult ContactUsForm()
+        {
+            return View();
+        }
+
+        [HttpPost, AllowAnonymous, ValidateAntiForgeryToken]
+        public async Task<IActionResult> ContactUsForm(ContactUsViewModel contactObject)
+        {
+            return View();
+        }
+    }
+}
