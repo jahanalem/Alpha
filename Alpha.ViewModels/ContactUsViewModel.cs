b0c7d7aa6ed91ad7f0e221e259319088fdf38fdd
jahanalem <s.r.alem19@gmail.com>
2020-03-14 03:08:22 +0100

    contact form

---
+++ Alpha.ViewModels/ContactUsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alpha.Models;
+
+namespace Alpha.ViewModels
+{
+    public class ContactUsViewModel
+    {
+        public ContactUs ContactUs { get; set; }
+    }
+}
