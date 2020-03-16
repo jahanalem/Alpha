4b7c1fedb35a4c39deb8cec883ef0600c450aa11
jahanalem <s.r.alem19@gmail.com>
2020-03-16 02:57:00 +0100

    Identity-01

---
+++ Alpha.DataAccess/Migrations/20200315014757_Add_IpAddress.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Alpha.DataAccess.Migrations
+{
+    public partial class Add_IpAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IpAddress",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IpAddress",
+                table: "User");
+        }
+    }
+}
--- Alpha.Infrastructure/CustomUserValidator.cs
+++ Alpha.Infrastructure/CustomUserValidator.cs
@@ -1,25 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Alpha.Models.Identity;
 using Microsoft.AspNetCore.Identity;
 
 namespace Alpha.Infrastructure
 {
-    public class CustomUserValidator : IUserValidator<User>
+    public class CustomUserValidator : UserValidator<User>
     {
-        public Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user)
+        public override async Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user)
         {
-            if (user.Email.ToLower().EndsWith("@example.com"))
-            {
-                return Task.FromResult(IdentityResult.Success);
-            }
-            else
-            {
-                return Task.FromResult(IdentityResult.Failed(new IdentityError
-                {
-                    Code = "EmailDomainError",
-                    Description = "Only example.com email addresses are allowed"
-                }));
-            }
+            IdentityResult result = await base.ValidateAsync(manager, user);
+
+            List<IdentityError> errors = result.Succeeded ? new List<IdentityError>() : result.Errors.ToList();
+            
+            //if (!user.Email.ToLower().EndsWith("@example.com"))
+            //{
+            //    errors.Add(new IdentityError
+            //    {
+            //        Code = "EmailDomainError",
+            //        Description = "Only example.com email addresses are allowed"
+            //    });
+            //}
+            return errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray());
         }
     }
 }
