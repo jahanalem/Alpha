0ccb3947015ea40e25a53b9ffdb2a0bb650c36fa
jahanalem <s.r.alem19@gmail.com>
2020-04-01 19:46:07 +0200

    Identity, Confirm, Reset

--- Alpha.Infrastructure/Email/EmailTemplatesSettings.cs
+++ Alpha.Infrastructure/Email/EmailTemplatesSettings.cs
@@ -4,28 +4,28 @@ using System.Text;
 
 namespace Alpha.Infrastructure.Email
 {
-    public static class EmailSettings
+    public static class EmailTemplatesSettings
     {
         public const string HtmlTemplateImpressum = "Impressum.html";
         public const string EmailSenderDomain = @"gmail.com";
         public static class AccountActivation
         {
             public const string SenderAddressPrefix = "test19application";
-            public const string Subject = @"FE-Portal: Bitte aktivieren Sie Ihren Zugang";
+            public const string Subject = @"Alpha: Bitte aktivieren Sie Ihren Zugang";
             public const string HtmlTemplateName = "Confirm_Account_Registration.html";
         }
 
         public static class PasswordForgot
         {
-            public const string SenderAddressPrefix = "nicht-antworten";
+            public const string SenderAddressPrefix = "test19application";
             public const string Subject = @"Alpha: Password reset";
             public const string HtmlTemplateName = "Reset_Password.html";
         }
 
-        public static class EmailConfirm
+        public static class EmailConfirmation
         {
-            public const string SenderAddressPrefix = "nicht-antworten";
-            public const string Subject = @"Alpha: Bitte bestätigen Sie Ihre E-Mail-Adresse";
+            public const string SenderAddressPrefix = "test19application";
+            public const string Subject = @"Alpha: Please confirm the E-Mail-Address";
             public const string HtmlTemplateName = "Confirm_Email.html";
         }
 
