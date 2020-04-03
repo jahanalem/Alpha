7fba4c3a54416e8f1db1f1afbcd76d499b295e1c
jahanalem <s.r.alem19@gmail.com>
2020-04-03 23:05:44 +0200

    template-001

--- Alpha.Infrastructure/Email/EmailTemplatesSettings.cs
+++ Alpha.Infrastructure/Email/EmailTemplatesSettings.cs
@@ -10,33 +10,36 @@ namespace Alpha.Infrastructure.Email
         public const string EmailSenderDomain = @"gmail.com";
         public static class AccountActivation
         {
-            public const string Subject = @"Alpha: Bitte aktivieren Sie Ihren Zugang";
+            public const string Subject = @"Green Codes: Please activate your access";
             public const string HtmlTemplateName = "Confirm_Account_Registration.html";
         }
 
         public static class PasswordForgot
         {
-            public const string Subject = @"Alpha: Password reset";
+            public const string Subject = @"Green Codes: Password reset";
             public const string HtmlTemplateName = "Reset_Password.html";
         }
 
         public static class EmailConfirmation
         {
             public const string SenderAddressPrefix = "test19application";
-            public const string Subject = @"Alpha: Please confirm the E-Mail-Address";
+            public const string Subject = @"Green Codes: Please confirm the Email Address";
             public const string HtmlTemplateName = "Confirm_Email.html";
         }
 
         public static class ReplaceToken
         {
             public const string ImagesUrl = "{{imagesUrl}}";
-            public const string UserName = "{{userName}}";
-            public const string Link = "{{link}}";
+            public const string UserName = "{{name}}";
+            public const string ActionUrl = "{{actionUrl}}";
             public const string Impressum = "{{impressum}}";
             public const string RequestId = "{{RequestId}}";
             public const string TimeOfError = "{{TimeOfError}}";
             public const string DetailOfError = "{{DetailOfError}}";
             public const string UserEmail = "{{UserEmail}}";
+            public const string WebsiteUrl = "{{websiteUrl}}";
+            public const string WebsiteName = "{{websiteName}}";
+            public const string SupportUrl = "{{supportUrl}}";
         }
     }
 }
