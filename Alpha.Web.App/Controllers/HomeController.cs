c3303ffac2ab1e2a16f3e4eaecfee57c95490e79
jahanalem <s.r.alem19@gmail.com>
2020-04-06 15:21:20 +0200

    Global exception handling

--- Alpha.Web.App/Controllers/HomeController.cs
+++ Alpha.Web.App/Controllers/HomeController.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using Alpha.LoggerService;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Logging;
 using Alpha.Web.App.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Fluent;
+
+//using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Alpha.Web.App.Controllers
 {
     public class HomeController : BaseController
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILoggerManager _logger;
+        public HomeController(ILoggerManager logger)
         {
             _logger = logger;
         }
@@ -25,6 +31,12 @@ namespace Alpha.Web.App.Controllers
             return View();
         }
 
+
+        public IActionResult Error(string requestId, string timeOfError)
+        {
+            var t = DateTime.ParseExact(timeOfError, "yyyy-MM-dd_HH-mm-ss", CultureInfo.InvariantCulture);
+            return View(new ErrorViewModel { RequestId = requestId, TimeOfError = t });
+        }
         //public IActionResult Privacy()
         //{
         //    return View();
---
+++ Alpha.Web.App/GlobalErrorHandling/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Alpha.LoggerService;
+using Alpha.Models.GlobalErrorHandling;
+using Alpha.Web.App.GlobalErrorHandling.CustomExceptionMiddleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace Alpha.Web.App.GlobalErrorHandling.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILoggerManager logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error."
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
