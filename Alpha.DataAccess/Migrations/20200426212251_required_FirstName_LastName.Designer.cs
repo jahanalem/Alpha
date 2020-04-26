4ee9af37a222c3282e466226cf754d012c20bde6
jahanalem <s.r.alem19@gmail.com>
2020-04-26 23:26:25 +0200

    Google,Facebook, user

--- Alpha.DataAccess/Migrations/20200426212251_required_FirstName_LastName.Designer.cs
+++ Alpha.DataAccess/Migrations/20200426212251_required_FirstName_LastName.Designer.cs
@@ -4,14 +4,16 @@ using Alpha.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Alpha.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200426212251_required_FirstName_LastName")]
+    partial class required_FirstName_LastName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
