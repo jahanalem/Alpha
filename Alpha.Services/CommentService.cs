7f0c26d739e5f1538f84836514a1217a9b0751be
jahanalem <s.r.alem19@gmail.com>
2020-04-28 00:09:31 +0200

    get pic from external loginprovider

--- Alpha.Services/CommentService.cs
+++ Alpha.Services/CommentService.cs
@@ -11,17 +11,23 @@ using Alpha.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
-
+using Microsoft.AspNetCore.Identity;
 namespace Alpha.Services
 {
     public class CommentService : BaseService<ICommentRepository, Comment>, ICommentService
     {
         private ICommentRepository _commentRepository;
         private ApplicationDbContext _dbContext;
-        public CommentService(ICommentRepository commentRepository, ApplicationDbContext applicationDbContext) : base(commentRepository)
+        private UserManager<User> _userManager;
+
+        public CommentService(ICommentRepository commentRepository,
+            ApplicationDbContext applicationDbContext,
+            UserManager<User> userManager) : base(commentRepository)
         {
             _commentRepository = commentRepository;
             _dbContext = applicationDbContext;
+            _userManager = userManager;
+
         }
 
         public async Task<List<Comment>> GetComments(int articleId)
@@ -32,11 +38,20 @@ namespace Alpha.Services
                      join u in _dbContext.Users on c.UserId equals u.Id into cu
                      from u in cu.DefaultIfEmpty()
                      where c.ArticleId == articleId
-                     select new { Comment = c, PublicUserName = u.DisplayName };
+                     select new
+                     {
+                         Comment = c,
+                         PublicUserName = u.DisplayName,
+                     };
             var queryResults = await q0.OrderByDescending(t => t.Comment.CreatedDate).ToListAsync();
             foreach (var r in queryResults)
             {
                 r.Comment.PublicUserName = r.PublicUserName ?? "Anonymous";
+                //var user = await _userManager.FindByIdAsync(r.Comment.UserId.ToString());
+                var claims = await _userManager.GetClaimsAsync(new User { Id = r.Comment.UserId.Value });
+                var claim = claims.FirstOrDefault(x => x.Type == Alpha.Infrastructure.AdditionalClaimTypes.Picture);
+                var picture = claim != null ? claim.Value : null;
+                r.Comment.UserPictureUrl = picture;
                 comments.Add(r.Comment);
             }
             //var query = await _commentRepository.Instance().Where(c => c.ArticleId == articleId)
