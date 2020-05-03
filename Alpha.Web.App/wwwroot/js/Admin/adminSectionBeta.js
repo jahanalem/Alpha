25c268d161f2082f31b80db322cf61cc4df6e67f
jahanalem <s.r.alem19@gmail.com>
2020-05-03 20:02:38 +0200

    tinymce 5.2.2

--- Alpha.Web.App/wwwroot/js/Admin/adminSectionBeta.js
+++ Alpha.Web.App/wwwroot/js/Admin/adminSectionBeta.js
@@ -14,7 +14,7 @@ function tinymce_getContentLength() {
 }
 
 tinymce.init({
-    selector: '#summaryId',
+    selector: 'textarea#summaryId',
     max_chars: 255, // max. allowed chars
     plugins: "paste",
     setup: function (ed) {
@@ -52,18 +52,86 @@ tinymce.init({
 
 
 
+
+tinymce.init({
+    selector: 'textarea#descId',
+    plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+    imagetools_cors_hosts: ['picsum.photos'],
+    menubar: 'file edit view insert format tools table help',
+    toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+    toolbar_sticky: true,
+    autosave_ask_before_unload: true,
+    autosave_interval: "30s",
+    autosave_prefix: "{path}{query}-{id}-",
+    autosave_restore_when_empty: false,
+    autosave_retention: "2m",
+    image_advtab: true,
+    content_css: '//www.tiny.cloud/css/codepen.min.css',
+    link_list: [
+        { title: 'My page 1', value: 'http://www.tinymce.com' },
+        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+    ],
+    image_list: [
+        { title: 'My page 1', value: 'http://www.tinymce.com' },
+        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+    ],
+    image_class_list: [
+        { title: 'None', value: '' },
+        { title: 'Some class', value: 'class-name' }
+    ],
+    importcss_append: true,
+    height: 400,
+    file_picker_callback: function (callback, value, meta) {
+        /* Provide file and text for the link dialog */
+        if (meta.filetype === 'file') {
+            callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+        }
+
+        /* Provide image and alt text for the image dialog */
+        if (meta.filetype === 'image') {
+            callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+        }
+
+        /* Provide alternative source and posted for the media dialog */
+        if (meta.filetype === 'media') {
+            callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+        }
+    },
+    templates: [
+        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+        { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+        { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+    ],
+    template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+    template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+    height: 600,
+    image_caption: true,
+    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+    noneditable_noneditable_class: "mceNonEditable",
+    toolbar_mode: 'sliding',
+    contextmenu: "link image imagetools table",
+
+    setup: function (editor) {
+        editor.on('change', function () {
+            tinymce.triggerSave();
+        });
+    }
+});
+
+
+
 //tinymce.init({ selector: '#summaryId', max_chars:"4" });
 
-tinymce.init(
-    {
-        selector: '#descId',
-        setup: function (editor) {
-            editor.on('change', function () {
-                tinymce.triggerSave();
-            });
-        }
-    }
-);
+//tinymce.init(
+//    {
+//        selector: '#descId',
+//        setup: function (editor) {
+//            editor.on('change', function () {
+//                tinymce.triggerSave();
+//            });
+//        }
+//    }
+//);
 
 
 $(document).ready(function () {
@@ -82,67 +150,67 @@ $(document).ready(function () {
 
 
 
-$(document).ready(function (e) {
+//$(document).ready(function (e) {
 
-    $("#usersDataTable").DataTable({
-        "processing": true, // for show progress bar
-        "serverSide": true, // for process server side
-        "filter": true, // this is for disable filter (search box)
-        "orderMulti": false, // for disable multiple column at once
-        "ajax": {
-            "url": "/Admin/Users/LoadData",
-            "type": "POST",
-            "datatype": "json"
-        },
-        "columnDefs":
-            [{
-                "targets": [0],
-                "visible": false,
-                "searchable": false
-            }],
-        "columns": [
-            { "data": "Id", "name": "Id", "autoWidth": true },
-            { "data": "UserName", "name": "UserName", "autoWidth": true },
-            { "data": "Email", "name": "Email", "autoWidth": true },
-            { "data": "RoleNames", "name": "RoleNames", "autoWidth": true },
-            {
-                "render": function (data, type, full, meta) { return '<a class="btn btn-info" href="/Admin/Users/Edit/' + full.Id + '">Edit</a>'; }
-            },
-            {
-                data: null, render: function (data, type, row) {
-                    return "<a href='#' class='btn btn-danger' onclick=DeleteData('" + row.Id + "'); >Delete</a>";
-                }
-            }
-        ]
+//    $("#usersDataTable").DataTable({
+//        "processing": true, // for show progress bar
+//        "serverSide": true, // for process server side
+//        "filter": true, // this is for disable filter (search box)
+//        "orderMulti": false, // for disable multiple column at once
+//        "ajax": {
+//            "url": "/Admin/Users/LoadData",
+//            "type": "POST",
+//            "datatype": "json"
+//        },
+//        "columnDefs":
+//            [{
+//                "targets": [0],
+//                "visible": false,
+//                "searchable": false
+//            }],
+//        "columns": [
+//            { "data": "Id", "name": "Id", "autoWidth": true },
+//            { "data": "UserName", "name": "UserName", "autoWidth": true },
+//            { "data": "Email", "name": "Email", "autoWidth": true },
+//            { "data": "RoleNames", "name": "RoleNames", "autoWidth": true },
+//            {
+//                "render": function (data, type, full, meta) { return '<a class="btn btn-info" href="/Admin/Users/Edit/' + full.Id + '">Edit</a>'; }
+//            },
+//            {
+//                data: null, render: function (data, type, row) {
+//                    return "<a href='#' class='btn btn-danger' onclick=DeleteData('" + row.Id + "'); >Delete</a>";
+//                }
+//            }
+//        ]
 
-    });
-});
-
-
-function DeleteData(Id) {
-    if (confirm("Are you sure you want to delete ...?")) {
-        Delete(Id);
+//    });
+//});
 
 
-    }
-    else {
-        return false;
-    }
-}
+//function DeleteData(Id) {
+//    if (confirm("Are you sure you want to delete ...?")) {
+//        Delete(Id);
 
 
-function Delete(Id) {
-    var url = "/admin/Users/Delete";
-    $.post(url, { ID: Id }, function (data) {
-        if (data) {
-            oTable = $('#usersDataTable').DataTable();
-            oTable.draw();
-        }
-        else {
-            alert("Something Went Wrong!");
-        }
-    });
-}
+//    }
+//    else {
+//        return false;
+//    }
+//}
+
+
+//function Delete(Id) {
+//    var url = "/admin/Users/Delete";
+//    $.post(url, { ID: Id }, function (data) {
+//        if (data) {
+//            oTable = $('#usersDataTable').DataTable();
+//            oTable.draw();
+//        }
+//        else {
+//            alert("Something Went Wrong!");
+//        }
+//    });
+//}
 
 
 
