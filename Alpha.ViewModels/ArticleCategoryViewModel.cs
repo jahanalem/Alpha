22d60d75fbe7f252ac22fa02160095348018328b
jahanalem <s.r.alem19@gmail.com>
2020-05-31 03:13:40 +0200

    article category 01

---
+++ Alpha.ViewModels/ArticleCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Alpha.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alpha.ViewModels
+{
+    public class ArticleCategoryViewModel : BaseViewModel
+    {
+        public ArticleCategory Category { get; set; }
+        public List<ArticleCategory> Parents { get; set; }
+    }
+}
