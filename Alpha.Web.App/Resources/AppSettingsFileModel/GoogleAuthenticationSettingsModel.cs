3f2c168239bffd697bd46be87e8a596ff3ce63e4
Said Roohullah Allem <s.r.alem19@gmail.com>
2020-12-31 19:36:04 +0100

    add styles for live search

---
+++ Alpha.Web.App/Resources/AppSettingsFileModel/GoogleAuthenticationSettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alpha.Web.App.Resources.AppSettingsFileModel
+{
+    public class GoogleAuthenticationSettingsModel
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
