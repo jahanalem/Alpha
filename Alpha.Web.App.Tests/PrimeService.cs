1cd6104582dda9a699220baec34e4030aa1f5bae
Said Roohullah Allem <s.r.alem19@gmail.com>
2020-12-31 19:36:04 +0100

    add styles for live search

---
+++ Alpha.Web.App.Tests/PrimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpha.Web.App.Tests
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            return false;
+            //throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
